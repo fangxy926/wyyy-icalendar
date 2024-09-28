622aa6473a12ec895092bedc30aa6c1f5bc313fe
fangxy926 <1515243746@qq.com>
2024-09-28 11:25:53 +0800

    update

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from icalendar import Calendar, Event
-from datetime import datetime, timedelta, timezone
+from icalendar import Calendar, Event, Timezone, TimezoneStandard
+from datetime import datetime, timedelta
 import uuid
-import time
-
-# 北京时间
-tz_utc_8 = timezone(timedelta(hours=8))
 
 
-# Function to create an event
+# 添加事件
 def create_event(summary, start, end):
     event = Event()
     event.add('summary', summary)
@@ -15,14 +11,33 @@ def create_event(summary, start, end):
     event.add('dtend', end)
     # UID保证唯一
     event['uid'] = str(uuid.uuid1())
-
     return event
 
 
 # Create calendar
 cal = Calendar()
+# 北京时间
+timezone = Timezone()
+timezone.add('tzid', 'Asia/Shanghai')
+tz_standard = TimezoneStandard()
+tz_standard.add('tzname', 'CST')
+tz_standard.add('dtstart', datetime(1970, 1, 1, 0, 0, 0))
+tz_standard.add('tzoffsetfrom', timedelta(hours=8))
+tz_standard.add('tzoffsetto', timedelta(hours=8))
+timezone.add_component(tz_standard)
+cal.add_component(timezone)
 
 year = 2024
+
+am_start_h = 8
+am_start_m = 0
+am_end_h = 12
+am_end_m = 0
+
+pm_start_h = 13
+pm_start_m = 30
+pm_end_h = 16
+pm_end_m = 30
 
 # 元旦
 
@@ -41,9 +56,10 @@ year = 2024
 
 
 # 国庆节
-cal.add_component(create_event('国庆节假期', datetime(year, 10, 1, tzinfo=tz_utc_8),
-                               datetime(year, 10, 3, tzinfo=tz_utc_8)))
-# cal.add_component(create_event('9月29日上班', datetime(year, 9, 29), datetime(year, 9, 30)))
+cal.add_component(create_event('国庆节假期', datetime(year, 10, 1),
+                               datetime(year, 10, 3)))
+cal.add_component(create_event('上班', datetime(year, 9, 30, am_start_h, am_start_m),
+                               datetime(year, 9, 30, pm_end_h, pm_end_m)))
 # cal.add_component(create_event('10月12日上班', datetime(year, 10, 12), datetime(year, 10, 13)))
 
 # Save the calendar to a file
